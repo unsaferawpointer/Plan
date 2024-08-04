4af14b7538ec8c43846ff6d3793e3ef7e7e58745
Anton Cherkasov <unsaferawpointer@gmail.com>
2024-08-04 18:56:18 +0300

    Initial Commit

---
+++ Plan/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Plan
+//
+//  Created by Anton Cherkasov on 04.08.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
