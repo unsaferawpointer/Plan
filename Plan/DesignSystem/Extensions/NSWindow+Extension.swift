1385712f33b19ea4aa2a03f1ccaeacdcd921647f
Anton Cherkasov <unsaferawpointer@gmail.com>
2024-10-26 21:00:31 +0300

    [fix]: Fix document windows

--- Plan/DesignSystem/Extensions/NSWindow+Extension.swift
+++ Plan/DesignSystem/Extensions/NSWindow+Extension.swift
@@ -9,7 +9,7 @@ import Cocoa
 
 extension NSWindow {
 
-	static let main: NSWindow = {
+	static func makeMain() -> NSWindow {
 		let window = NSWindow()
 		window.minSize = .init(width: 640, height: 480)
 		window.styleMask = [
@@ -22,13 +22,11 @@ extension NSWindow {
 		]
 		window.isRestorable = true
 		window.center()
-		window.setFrameAutosaveName("main_window")
 		window.titleVisibility = .visible
 		window.titlebarAppearsTransparent = false
 		window.tabbingMode = .preferred
 		window.titlebarSeparatorStyle = .automatic
 		window.toolbarStyle = .unified
-		window.identifier = .init("main_window")
 		return window
-	}()
+	}
 }
